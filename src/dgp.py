e4cc4ec200ccb583cf30b4f3831b3439a8066a6c
Lorenzo Costa <lorenzocosta2001@gmail.com>
2026-02-06 13:41:01 -0500

    dgp + tests

--- src/dgp.py
+++ src/dgp.py
@@ -3,23 +3,63 @@ from scipy import stats
 from scipy.special import expit
 
 # TODO:
-# - extend gaussian network generation to more than two 
+# - extend gaussian network generation to more than two
+
 
 class BaseDPG:
     def __init__(self, rng=None):
         if rng is None:
             rng = np.random.default_rng()
         self.rng = rng
+
     def generate(self):
         raise NotImplementedError("Subclasses should implement this!")
+
     def name(self):
         raise NotImplementedError("Subclasses should implement this!")
 
 
-class GaussianNetwork(BaseDPG):
+class CopulaDGP:
+    """Base class for Copula Data Generating Processes."""
+
+    def __init__(self, rng):
+        self.rng = rng
+
+    def _generate_correlated_gaussians(self):
+        """
+        Sample the latent vector for Gaussian Copula model (i.e. q in the notes)
+        using a Gaussian Copula with correlation matrix R.
+        R has block structure [[I, sI], [sI, I]].
+
+        We simulate this by generating z ~ N(0, I) and x = sz + sqrt(1-s^2)e.
+        """
+
+        # # shape: (n, k)
+        # cov_matrix = np.block([
+        #     [self.sigma * np.eye(self.k),  np.eye(self.k)],
+        #     [np.eye(self.k), self.sigma * np.eye(self.k)]
+        # ])
+        # q = self.rng.multivariate_normal(np.zeros(2*self.k), cov_matrix, size=self.n)
+        # qz, qx = q[:, :self.k], q[:, self.k:]
+
+        # more efficient version uses the formula
+        # z \sim N(0, I), x = s * z + sqrt(1-s^2) * e
+        z = self.rng.normal(size=(self.n, self.k))
+        e = self.rng.normal(size=(self.n, self.k))
+        if self.sigma == 1:
+            x = z
+        elif self.sigma == -1:
+            x = -z
+        else:
+            x = self.sigma * z + np.sqrt(1 - self.sigma**2) * e
+
+        return z, x
+
+
+class GaussianNetwork(BaseDPG, CopulaDGP):
     """
-    Network Data Generating Process using Gaussian Copulas.
-    
+    Weighted network DGP with Gaussian weights on edges.
+
     Allows for arbitrary marginal distributions for the latent positions Z and X,
     while maintaining a gaussian copula correlation structure.
 
@@ -31,133 +71,93 @@ class GaussianNetwork(BaseDPG):
         Dimensionality of the latent space.
     sigma : float
         Correlation parameter for the Gaussian copula (-1 to 1).
-    marginal_z : scipy.stats.rv_continuous, optional
-        Distribution for latent Z (default: standard normal).
-    marginal_x : scipy.stats.rv_continuous, optional
-        Distribution for latent X (default: standard normal).
-    marginal_z_params : dict, optional
-        Parameters for marginal_z (e.g., {'loc': 0, 'scale': 1}).
-    marginal_x_params : dict, optional
-        Parameters for marginal_x (e.g., {'df': 5} for t-distribution).
+    marginals : dict, scipy.stats.rv_continuous
+        Marginal distributions for the latent variables. If dict it should have 'z' and 'x' keys.
     edge_var : float, optional
         Variance of the edges (default is 1).
     rng : np.random.Generator, optional
         Random number generator.
     """
-    def __init__(self, n, k, sigma, 
-                 marginal_z=stats.norm, 
-                 marginal_x=None,
-                 marginal_z_params=None, 
-                 marginal_x_params=None,
-                 edge_var=1, rng=None,
-                 **args):
 
+    def __init__(self, n, k, sigma, marginals=stats.norm, edge_var=1, rng=None, **args):
+        # note here by multiple inheritance BaseDPG init will be called
         super().__init__(rng=rng)
 
         self.n = n
         self.k = k
         self.sigma = sigma
         self.edge_var = edge_var
-        
-        # Marginals
-        self.marginal_z = marginal_z
-        if marginal_x is None:
-            self.marginal_x = marginal_z
+
+        self._type_check(marginals)
+        self.marginals = marginals
+
+        if isinstance(marginals, dict):
+            self.marginal_z = marginals.get("z", stats.norm)
+            self.marginal_x = marginals.get("x", stats.norm)
         else:
-            self.marginal_x = marginal_x
-            
-        self.marginal_z_params = marginal_z_params if marginal_z_params else {}
-        self.marginal_x_params = marginal_x_params if marginal_x_params else {}
-            
-    def __repr__(self):
-        return (f"GaussianNetwork(n={self.n}, k={self.k}, sigma={self.sigma}, "
-                f"edge_var={self.edge_var}, "
-                f"marginal_z={self.marginal_z}, marginal_x={self.marginal_x})")
+            self.marginal_z = marginals
+            self.marginal_x = marginals
 
     def name(self):
         return "GaussianNetwork"
 
-    def _generate_correlated_gaussians(self):
-        """
-        Sample the latent vector for Gaussian Copula model (i.e. q in the notes)
-        using a Gaussian Copula with correlation matrix R.
-        R has block structure [[I, sI], [sI, I]]. 
-        
-        We simulate this by generating z ~ N(0, I) and x = sz + sqrt(1-s^2)e.
-        """
-        
-        # # shape: (n, k)
-        # cov_matrix = np.block([
-        #     [self.sigma * np.eye(self.k),  np.eye(self.k)],
-        #     [np.eye(self.k), self.sigma * np.eye(self.k)]
-        # ])
-        # q = self.rng.multivariate_normal(np.zeros(2*self.k), cov_matrix, size=self.n)
-        # qz, qx = q[:, :self.k], q[:, self.k:]
-        
-        # more efficient version uses the formula 
-        # z \sim N(0, I), x = s * z + sqrt(1-s^2) * e
-        z = self.rng.normal(size=(self.n, self.k))
-        e = self.rng.normal(size=(self.n, self.k))
-        if self.sigma == 1:
-            x = z
-        elif self.sigma == -1:
-            x = -z
-        else:
-            x = self.sigma * z + np.sqrt(1 - self.sigma**2) * e
-            
-        return z, x
-
     def generate(self):
         # sample latent gaussian vectors
         q_z, q_x = self._generate_correlated_gaussians()
-        
+
         # convert to uniform via CDF of standard normal
         u_z = stats.norm.cdf(q_z)
         u_x = stats.norm.cdf(q_x)
 
         # apply inverse CDF of desired marginals
-        Z = self.marginal_z.ppf(u_z, **self.marginal_z_params)
-        X = self.marginal_x.ppf(u_x, **self.marginal_x_params)
-        
+        Z = self.marginal_z.ppf(u_z)
+        X = self.marginal_x.ppf(u_x)
+
         # generate adj matrices
-        # note, for some marginals (eg heavy tails) the dot product can be very large, 
+        # note, for some marginals (eg heavy tails) the dot product can be very large,
         # maybe it is worth looking into ways of somehow normalising this
         expected_A = Z @ Z.T
         expected_B = X @ X.T
 
         A = self.rng.normal(loc=expected_A, scale=self.edge_var)
         B = self.rng.normal(loc=expected_B, scale=self.edge_var)
-        
+
         # Symmetrise
         A = (A + A.T) / 2
         B = (B + B.T) / 2
-        
+
         return A, B, Z, X
 
-    # def generate_gaussian_data(self):
-    #     I_k = np.eye(self.k)
-    #     R = np.block([
-    #             [I_k,          self.sigma * I_k],
-    #             [self.sigma * I_k,  I_k        ]
-    #         ])
+    def _type_check(self, marginals):
+        # for now only continuous marginals allowed
+        if isinstance(marginals, dict):
+            # stats._distn_infrastructure.rv_continuous_frozen happens when feeding an instantiated obeject (e.g. stats.norm())
+            if not isinstance(
+                marginals.get("z", stats.norm),
+                (stats.rv_continuous, stats._distn_infrastructure.rv_continuous_frozen),
+            ):
+                raise ValueError("Invalid marginal distributions")
+            if not isinstance(
+                marginals.get("x", stats.norm),
+                (stats.rv_continuous, stats._distn_infrastructure.rv_continuous_frozen),
+            ):
+                raise ValueError("Invalid marginal distributions")
+        else:
+            if not isinstance(marginals, stats.rv_continuous):
+                raise ValueError("Invalid marginal distribution")
 
-    #     q = self.rng.multivariate_normal(np.zeros(2*self.k), R, size=self.n)
-    #     Z = q[:, :self.k]
-    #     X = q[:, self.k:]
+    def __repr__(self):
+        return (
+            f"GaussianNetwork(n={self.n}, k={self.k}, sigma={self.sigma}, "
+            f"edge_var={self.edge_var}, "
+            f"marginal_z={self.marginal_z}, marginal_x={self.marginal_x})"
+        )
 
-    #     A = self.rng.normal(loc=Z @ Z.T, scale=self.edge_var)
-    #     B = self.rng.normal(loc=X @ X.T, scale=self.edge_var)
-    #     # symmetrise
-    #     A = (A + A.T) / 2 
-    #     B = (B + B.T) / 2
-        
-    #     return A, B, Z, X
-    
 
-class BernoulliNetwork(BaseDPG):
+class BernoulliNetwork(BaseDPG, CopulaDGP):
     """
     Network Data Generating Process using Bernoulli likelihood.
-    
+
     Allows for arbitrary marginal distributions for the latent positions Z and X,
     while maintaining a gaussian copula correlation structure. Dot product of latent
     positions models the log-odds of entries of the adjacency matrix.
@@ -170,99 +170,51 @@ class BernoulliNetwork(BaseDPG):
         Dimensionality of the latent space.
     sigma : float
         Correlation parameter for the Gaussian copula (-1 to 1).
-    marginal_z : scipy.stats.rv_continuous, optional
-        Distribution for latent Z (default: standard normal).
-    marginal_x : scipy.stats.rv_continuous, optional
-        Distribution for latent X (default: standard normal).
-    marginal_z_params : dict, optional
-        Parameters for marginal_z (e.g., {'loc': 0, 'scale': 1}).
-    marginal_x_params : dict, optional
-        Parameters for marginal_x (e.g., {'df': 5} for t-distribution).
+    marginals : dict, scipy.stats.rv_continuous
+        Marginal distributions for the latent variables. If dict it should have 'z' and 'x' keys.
     edge_var : float, optional
         Variance of the edges (default is 1).
     rng : np.random.Generator, optional
         Random number generator.
     """
-    def __init__(self, n, k, sigma, 
-                 marginal_z=stats.norm, 
-                 marginal_x=None,
-                 marginal_z_params=None, 
-                 marginal_x_params=None,
-                 edge_var=1, rng=None,
-                 **args):
 
+    def __init__(self, n, k, sigma, marginals=stats.norm, edge_var=1, rng=None, **args):
+        # note here by multiple inheritance BaseDPG init will be called
         super().__init__(rng=rng)
 
         self.n = n
         self.k = k
         self.sigma = sigma
         self.edge_var = edge_var
-        
-        # Marginals
-        self.marginal_z = marginal_z
-        if marginal_x is None:
-            self.marginal_x = marginal_z
+
+        self._type_check(marginals)
+        self.marginals = marginals
+
+        if isinstance(marginals, dict):
+            self.marginal_z = marginals.get("z", stats.norm)
+            self.marginal_x = marginals.get("x", stats.norm)
         else:
-            self.marginal_x = marginal_x
-        self.marginal_z_params = marginal_z_params if marginal_z_params else {}
-        self.marginal_x_params = marginal_x_params if marginal_x_params else {}
-            
-    def __repr__(self):
-        return (f"GaussianNetwork(n={self.n}, k={self.k}, sigma={self.sigma}, "
-                f"edge_var={self.edge_var}, "
-                f"marginal_z={self.marginal_z}, marginal_x={self.marginal_x})")
+            self.marginal_z = marginals
+            self.marginal_x = marginals
 
     def name(self):
         return "BernoulliNetwork"
 
-    def _generate_correlated_gaussians(self):
-        """
-        Sample the latent vector for Gaussian Copula model (i.e. q in the notes)
-        using a Gaussian Copula with correlation matrix R.
-        R has block structure [[I, sI], [sI, I]]. 
-        
-        We simulate this by generating z ~ N(0, I) and x = sz + sqrt(1-s^2)e.
-        """
-        
-        # # shape: (n, k)
-        # cov_matrix = np.block([
-        #     [self.sigma * np.eye(self.k),  np.eye(self.k)],
-        #     [np.eye(self.k), self.sigma * np.eye(self.k)]
-        # ])
-        # q = self.rng.multivariate_normal(np.zeros(2*self.k), cov_matrix, size=self.n)
-        # qz, qx = q[:, :self.k], q[:, self.k:]
-        
-        # more efficient version uses the formula 
-        # z \sim N(0, I), x = s * z + sqrt(1-s^2) * e
-        z = self.rng.normal(size=(self.n, self.k))
-        e = self.rng.normal(size=(self.n, self.k))
-        if self.sigma == 1:
-            x = z
-        elif self.sigma == -1:
-            x = -z
-        else:
-            x = self.sigma * z + np.sqrt(1 - self.sigma**2) * e
-            
-        return z, x
-
     def generate(self):
         # sample latent gaussian vectors
         q_z, q_x = self._generate_correlated_gaussians()
-        
+
         # convert to Uniform via CDF of standard normal (PIT)
         u_z = stats.norm.cdf(q_z)
         u_x = stats.norm.cdf(q_x)
 
         # apply Inverse CDF (PPF) to get desired marginals
-        Z = self.marginal_z.ppf(u_z, **self.marginal_z_params)
-        X = self.marginal_x.ppf(u_x, **self.marginal_x_params)
-        
-        # generate adj matrix using logistic function
-        logit_A = Z @ Z.T
-        logit_B = X @ X.T
+        Z = self.marginal_z.ppf(u_z)
+        X = self.marginal_x.ppf(u_x)
 
-        prob_A = expit(logit_A)
-        prob_B = expit(logit_B)
+        # generate adj matrix using logistic function
+        prob_A = expit(Z @ Z.T)
+        prob_B = expit(X @ X.T)
 
         # generate lower half and the sum to ensure symmetry
         A = np.tril(self.rng.binomial(1, prob_A), k=-1)
@@ -272,3 +224,28 @@ class BernoulliNetwork(BaseDPG):
         B = B + B.T
 
         return A, B, Z, X
+    
+    def __repr__(self):
+        return (
+            f"BernoulliNetwork(n={self.n}, k={self.k}, sigma={self.sigma}, "
+            f"edge_var={self.edge_var}, "
+            f"marginal_z={self.marginal_z}, marginal_x={self.marginal_x})"
+        )
+        
+    def _type_check(self, marginals):
+        # for now only continuous marginals allowed
+        if isinstance(marginals, dict):
+            # stats._distn_infrastructure.rv_continuous_frozen happens when feeding an instantiated obeject (e.g. stats.norm())
+            if not isinstance(
+                marginals.get("z", stats.norm),
+                (stats.rv_continuous, stats._distn_infrastructure.rv_continuous_frozen),
+            ):
+                raise ValueError("Invalid marginal distributions")
+            if not isinstance(
+                marginals.get("x", stats.norm),
+                (stats.rv_continuous, stats._distn_infrastructure.rv_continuous_frozen),
+            ):
+                raise ValueError("Invalid marginal distributions")
+        else:
+            if not isinstance(marginals, stats.rv_continuous):
+                raise ValueError("Invalid marginal distribution")
--- tests/test_dgp.py
+++ tests/test_dgp.py
@@ -2,7 +2,7 @@ import pytest
 import sys
 from pathlib import Path
 import numpy as np
-from src.dgp import GaussianNetwork
+from src.dgp import GaussianNetwork, BernoulliNetwork
 from scipy import stats
 import sys
 from pathlib import Path
@@ -13,35 +13,74 @@ sys.path.append(str(parent_dir))
 
 
 @pytest.mark.parametrize(
-    "n, k, sigma, edge_var, x_marginal, z_marginal, additional_args_x, additional_args_z",
+    "n, k, sigma, edge_var, marginals",
     [
-        (100, 5, 0.5, 1, stats.norm, stats.norm, {}, {}),
-        (200, 10, 0.1, 0.5, stats.norm, stats.norm, {}, {}),
-        (300, 15, 0.2, 0.8, stats.norm, stats.norm, {}, {}),
-        (100, 5, 0.5, 1, stats.expon, stats.expon, {}, {}),
-        (100, 10, 0.1, 0.5, stats.chi2, stats.chi2, {"df": 2}, {"df": 2}),
-        (100, 10, 0.1, 0.5, stats.norm, stats.chi2, {}, {"df": 2}),
-        (100, 10, 0.1, 0.5, stats.chi2, stats.expon, {"df": 2}, {}),
-        (100, 10, 0.1, 0.5, stats.expon, stats.norm, {}, {}),
+        (100, 5, 0.5, 1, {'z': stats.norm, 'x': stats.norm}),
+        (200, 10, 0.1, 0.5, {'z': stats.norm, 'x': stats.norm}),
+        (300, 15, 0.2, 0.8, {'z': stats.norm, 'x': stats.norm}),
+        (100, 5, 0.5, 1, {'z': stats.expon, 'x': stats.expon}),
+        (100, 10, 0.1, 0.5, {'z': stats.chi2(df=2), 'x': stats.chi2(df=2)}),
+        (100, 10, 0.1, 0.5, {'z': stats.norm, 'x': stats.chi2(df=2)}, {},),
+        (100, 10, 0.1, 0.5, {'z': stats.chi2(df=2), 'x': stats.expon}),
+        (100, 10, 0.1, 0.5, {'z': stats.expon, 'x': stats.norm}),
     ],
 )
 def test_GaussianNetwork(
-    n, k, sigma, edge_var, x_marginal, z_marginal, additional_args_x, additional_args_z
+    n, k, sigma, edge_var, marginals
 ):
     rng = np.random.default_rng(42)
     md = GaussianNetwork(
         n=n,
         k=k,
         sigma=sigma,
-        marginal_z=z_marginal,
-        marginal_x=x_marginal,
+        marginals=marginals,
         rng=rng,
         edge_var=edge_var,
-        marginal_z_params=additional_args_z,
-        marginal_x_params=additional_args_x,
     )
     A, B, X, Z = md.generate()
     assert A.shape == (n, n)
     assert B.shape == (n, n)
     assert X.shape == (n, k)
     assert Z.shape == (n, k)
+    assert np.allclose(A, A.T), "Adjacency matrix A is not symmetric"
+    assert np.allclose(B, B.T), "Adjacency matrix B is not symmetric"
+    assert np.diag(A).sum() == 0, "Adjacency matrix A has non-zero diagonal"
+    assert np.diag(B).sum() == 0, "Adjacency matrix B has non-zero diagonal"
+
+
+@pytest.mark.parametrize(
+    "n, k, sigma, edge_var, marginals",
+    [
+        (100, 5, 0.5, 1, {'z': stats.norm, 'x': stats.norm}),
+        (200, 10, 0.1, 0.5, {'z': stats.norm, 'x': stats.norm}),
+        (300, 15, 0.2, 0.8, {'z': stats.norm, 'x': stats.norm}),
+        (100, 5, 0.5, 1, {'z': stats.expon, 'x': stats.expon}),
+        (100, 10, 0.1, 0.5, {'z': stats.chi2(df=2), 'x': stats.chi2(df=2)}),
+        (100, 10, 0.1, 0.5, {'z': stats.norm, 'x': stats.chi2(df=2)}, {},),
+        (100, 10, 0.1, 0.5, {'z': stats.chi2(df=2), 'x': stats.expon}),
+        (100, 10, 0.1, 0.5, {'z': stats.expon, 'x': stats.norm}),
+    ],
+)
+def test_BernoulliNetwork(
+    n, k, sigma, edge_var, marginals
+):
+    rng = np.random.default_rng(42)
+    md = BernoulliNetwork(
+        n=n,
+        k=k,
+        sigma=sigma,
+        marginals=marginals,
+        rng=rng,
+        edge_var=edge_var,
+    )
+    A, B, X, Z = md.generate()
+    assert A.shape == (n, n)
+    assert B.shape == (n, n)
+    assert X.shape == (n, k)
+    assert Z.shape == (n, k)
+    assert np.allclose(A, A.T), "Adjacency matrix A is not symmetric"
+    assert np.allclose(B, B.T), "Adjacency matrix B is not symmetric"
+    assert set(np.unique(A)).issubset({0, 1}), "Adjacency matrix A is not binary"
+    assert set(np.unique(B)).issubset({0, 1}), "Adjacency matrix B is not binary"
+    assert np.diag(A).sum() == 0, "Adjacency matrix A has non-zero diagonal"
+    assert np.diag(B).sum() == 0, "Adjacency matrix B has non-zero diagonal"
