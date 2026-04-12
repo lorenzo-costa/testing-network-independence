a6fe5f15becfb611ae81925023c80e0a3e7e7df6
Lorenzo Costa <lorenzocosta2001@gmail.com>
2026-04-12 06:48:15 -0400

    s

--- src/solvers/weighted_network.py
+++ src/solvers/weighted_network.py
@@ -24,7 +24,7 @@ def ASE(A, k=2, rng=None, **kwargs):
     np.ndarray, np.ndarray
         Estimated latent positions, estimated eigenvalues
     """
-    k=2
+    k=4
     if rng is None:
         rng = np.random.default_rng()
 
